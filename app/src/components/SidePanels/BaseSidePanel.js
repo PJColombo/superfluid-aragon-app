4dff633cc1b4e39baacf1e48fa5a52ab18b8aa24
PJColombo <paulo.colombo@pm.me>
2022-01-03 03:38:16 +0100

    Fix side panel import

--- app/src/components/SidePanels/BaseSidePanel.js
+++ app/src/components/SidePanels/BaseSidePanel.js
@@ -1,22 +1,23 @@
-import { GU } from '@aragon/ui';
-import SidePanel from '@aragon/ui/dist/SidePanel';
 import React from 'react';
+import { GU, SidePanel } from '@aragon/ui';
 
-const BaseSidePanel = ({ title, panelState, disableMargin = false, children }) => (
-  <SidePanel
-    title={title}
-    opened={panelState.visible}
-    onClose={panelState.requestClose}
-    onTransitionEnd={panelState.onTransitionEnd}
-  >
-    <div
-      css={`
-        margin-top: ${disableMargin ? 0 : 4 * GU}px;
-      `}
+const BaseSidePanel = ({ title, panelState, disableMargin = false, children }) => {
+  return (
+    <SidePanel
+      title={title}
+      opened={panelState.visible}
+      onClose={panelState.requestClose}
+      onTransitionEnd={panelState.onTransitionEnd}
     >
-      {children}
-    </div>
-  </SidePanel>
-);
+      <div
+        css={`
+          margin-top: ${disableMargin ? 0 : 4 * GU}px;
+        `}
+      >
+        {children}
+      </div>
+    </SidePanel>
+  );
+};
 
 export default BaseSidePanel;
