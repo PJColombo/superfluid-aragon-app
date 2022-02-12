c56448c41c732aabab5e149139f406191d09b590
PJColombo <paulo.colombo@pm.me>
2022-02-12 20:59:18 -0300

    Verify chain id when checking test networks

--- app/src/helpers/network.js
+++ app/src/helpers/network.js
@@ -2,4 +2,7 @@ const TEST_NETWORKS = ['goerly', 'kovan', 'rinkeby', 'ropsten', 'mumbai', 'priva
 
 export const LOCAL_NETWORK_ID = 1337;
 
-export const isTestNetwork = network => TEST_NETWORKS.includes(network.type.toLowerCase());
+const TEST_NETWORK_IDS = [5, 42, 4, 3, 80001, LOCAL_NETWORK_ID];
+
+export const isTestNetwork = network =>
+  TEST_NETWORKS.includes(network.type.toLowerCase()) && TEST_NETWORK_IDS.includes(network.id);
