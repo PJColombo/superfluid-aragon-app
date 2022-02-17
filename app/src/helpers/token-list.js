76bbe69f1121032db5dfbee663ba159771b3a5d3
PJColombo <paulo.colombo@pm.me>
2022-02-17 01:50:18 -0300

    Add polygon token list

--- app/src/helpers/token-list.js
+++ app/src/helpers/token-list.js
@@ -2,6 +2,8 @@ import { isTestNetwork } from '.';
 
 const MAINNET_TOKEN_LIST = 'https://tokens.coingecko.com/uniswap/all.json';
 const XDAI_TOKEN_LIST = 'https://tokens.honeyswap.org/';
+const POLYGON_TOKEN_LIST =
+  'https://unpkg.com/quickswap-default-token-list@1.2.11/build/quickswap-default.tokenlist.json';
 
 export const getTokenListUrlByNetwork = network => {
   // Use mainnet token list for fetching token data such as logos
@@ -14,7 +16,9 @@ export const getTokenListUrlByNetwork = network => {
       return MAINNET_TOKEN_LIST;
     case 100:
       return XDAI_TOKEN_LIST;
+    case 137:
+      return POLYGON_TOKEN_LIST;
     default:
-      return null;
+      return MAINNET_TOKEN_LIST;
   }
 };
